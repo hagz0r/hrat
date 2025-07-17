af9d68ff89230fa0515372029631834a2857248b
hagz0r <goglesman13@gmail.com>
2025-07-18 00:47:48 +0500

    frontend and readme

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         ps.uvicorn
         ps.jinja2
         ps.python-multipart
-	ps.websockets
+        ps.websockets
       ]);
 
     in
@@ -25,16 +25,28 @@
         buildInputs = with pkgs; [
           pythonEnv
 
-
           cargo
           rustc
           pkg-config
+
+          llvmPackages.libclang
+          linuxHeaders
+          
+          # We are referencing glibc.dev in the shellHook, so it is good 
+          # practice to make it an explicit input, even though it's
+          # part of the standard environment.
+          glibc.dev
 
           xorg.libX11
           xorg.libxcb
         ];
 
         shellHook = ''
+          export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+
+          # Add include paths for BOTH the Linux headers and the C Standard Library
+          export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include"
+
           echo ""
           echo "--- hrat C&C Host Environment Activated ---"
           echo ""
